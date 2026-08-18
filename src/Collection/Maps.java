33d4921a45ccff6a59490ff790d738269ef4358f
kri_ shna <krishnakantranjan50@gmail.com>
2026-08-18 21:56:38 +0530

    Set and Map Done

---
+++ src/Collection/Maps.java
@@ -0,0 +1,23 @@
+package Collection;
+
+import java.util.*;
+
+public class Maps {
+    static void main(String[] args) {
+
+        Map<Integer, String> map = new HashMap<>();
+
+        map.put(101, "Krishna");
+        map.put(102, "Aditya");
+        map.put(103, "Rohit");
+
+
+        System.out.println(map.containsKey(101)); // true
+        System.out.println(map.get(102)); // Aditya
+
+
+
+
+
+    }
+}
---
+++ src/Collection/Sets.java
@@ -0,0 +1,30 @@
+package Collection;
+
+import java.util.*;
+
+public class Sets {
+    static void main(String[] args) {
+
+        Set<Integer> set = new HashSet<>();
+        set.add(10);
+        set.add(20);
+        set.add(15);
+
+        System.out.println(set.contains(20)); // true;
+
+
+        // Constructor
+
+        Set<Integer> set1 = new HashSet<>();
+
+        Set<Integer> set2 = new HashSet<>(100);
+
+        Set<Integer> set3 = new HashSet<>(100, 0.8f);
+
+        Set<Integer> set4 = new HashSet<>(List.of(1,2,3,4,5,6,7,8,9));
+
+
+
+
+    }
+}
